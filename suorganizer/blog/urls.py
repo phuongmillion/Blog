a0f877ae17f989925afb63fe0453093db303006e
Phuong Trieu Quoc <tquocphuong@tma.com.vn>
2019-12-06 16:38:28 +0700

    hanlde url partten for blog_post_detail.

--- suorganizer/blog/urls.py
+++ suorganizer/blog/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, register_converter
+from django.urls import path, re_path
 
 from . import views
 from .models import Post
@@ -7,5 +7,5 @@ app_name = 'blog'
 
 urlpatterns = [
     path('post/', views.post_list, name='post_list'),
-    path('blog/<int:year>/<int:month>/<slug:slug>', views.post_detail, name='post_detail')
+    re_path(r'^post/(?P<year>[0-9]{4})/(?<month>[0-9]{1,2})/(?P<slug>[\w\-]+)/$', views.post_detail, name='post_detail')
 ]
