b9fb0e152b104e32da3178b52a0f9c65f9561a86
phuongmillion <phuongmillion1@gmail.com>
2019-12-08 22:36:19 +0700

    added tag_form.

---
+++ suorganizer/organizer/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from.models import Tag
+
+
+class TagForm(forms.Form):
+    name = forms.CharField(max_length=31)
+    slug = forms.SlugField(max_length=31, help_text='A label for URL')
+
+    def save(self):
+        new_tag = Tag.objects.create(name=self.cleaned_data['name'], slug=self.cleaned_data['slug'])
+        return new_tag
